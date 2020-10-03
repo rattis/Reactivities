bc69ca7fddc9a584cf3f9e7af27a3d742c4276ac
Alan <rattis@outlook.com>
2020-10-03 16:22:58 -0300

    Fim do modulo 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -29,12 +29,15 @@ namespace API
         {
             services.AddDbContext<DataContext>( opt =>
             {
-
                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
-
-            }
-            
-            );
+            });
+            services.AddCors(opt =>{
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                }
+                );
+            });
             services.AddControllers();
             
         }
@@ -53,10 +56,15 @@ namespace API
 
             app.UseAuthorization();
 
+            app.UseCors("CorsPolicy");
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
+
+           
         }
     }
 }
